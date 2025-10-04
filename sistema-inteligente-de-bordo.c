4aa0bfa0eb8959e4f366b0697e771c09c5b3527c
Gabriel <fco.gabrielsilva2408@gmail.com>
2025-10-04 17:09:48 -0300

    initial commit

---
+++ sistema-inteligente-de-bordo.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+
+int main()
+{
+    // VARIAVEIS (start_var)
+    float vlr_gsl = 0, cst_abs = 0, qtd_gsl = 0, vlc_med = 0, auto = 0, temp = 0, qtd_tnq = 0;
+    int dslc = 0, cap_tnq = 0;
+    // (end_var)
+}
