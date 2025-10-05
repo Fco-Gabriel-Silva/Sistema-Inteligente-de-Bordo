9f7d4c99d0a00450b6fbf9c86fb8a468ca6229cc
Lorena Rodrigues <lorena.rodrigues.tech@gmail.com>
2025-10-05 15:53:07 -0300

    fix: correção da variavel auto => atnm

--- sistema-inteligente-de-bordo.c
+++ sistema-inteligente-de-bordo.c
@@ -3,7 +3,7 @@
 int main()
 {
     // VARIAVEIS (start_var)
-    float vlr_gsl = 0, cst_abs = 0, qtd_gsl = 0, vlc_med = 0, auto = 0, temp = 0, qtd_tnq = 0;
+    float vlr_gsl = 0, cst_abs = 0, qtd_gsl = 0, vlc_med = 0, atnm = 0, temp = 0, qtd_tnq = 0;
     int dslc = 0, cap_tnq = 0;
     // (end_var)
 }
@@ -12,7 +12,7 @@ int main()
     cst_abs = 78.00;    //REAL
     qtd_gsl = 3.00;     //LITRO
     vlc_med = 80.00;    //KM/H
-    auto = 20.00;       //KM/L
+    atnm = 20.00;       //KM/L
     temp = 1.5;         //HORAS
     qtd_tnq = 3.00;     //LITRO
     dslc = 210;         //KM
